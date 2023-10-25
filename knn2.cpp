74e2b6c4a8c1b26f94622c9c080dc62ba9c8ab4c
dorianGT <doriangrouteaupro@gmail.com>
2023-10-25 09:16:58 +0200

    Correct KNN
    Correction on KNN script

--- knn2.cpp
+++ knn2.cpp
@@ -1,5 +1,5 @@
 // C++ program to find groups of unknown
-// Points using K nearest neighbour algorithm.
+// Points using K nearest neighbor algorithm.
 #include <bits/stdc++.h>
 #include <iostream>
 #include <string>
@@ -8,37 +8,32 @@
 #include <vector>
 #include <map>
 using namespace std;
- 
+
 int numClasses = 9;
-string nameClasse = "E34";
+string className = "E34";
 std::map<string, int> map1 = {
     {"E34", 16},
     {"F0", 128},
     {"GFD", 100},
     {"SA", 90}};
 
-struct Point
+struct ImageData
 {
-    int val;                              // Groupe du point
-    vector<float> features;                // Vecteur de caractéristiques
-    double distance;                       // Distance depuis le point de test
+    int val;                              // Class of the image
+    vector<float> features;                // Feature vector
+    double distance;                       // Distance from the test ImageData
 
-    Point(int size) : features(size, 0) {} // Initialisation du vecteur de caractéristiques
+    ImageData(int size) : features(size, 0) {} // Initialize the feature vector
 };
- 
-// This function finds classification of point p using
-// k nearest neighbour algorithm. It assumes only two
-// groups and returns 0 if p belongs to group 0, else
-// 1 (belongs to group 1).
 
-
+// Function to calculate Euclidean distance between two feature vectors
 double calculateDistance(vector<float> features1, vector<float> features2)
 {
     double sum = 0.0;
-    // Assurez-vous que les deux vecteurs ont la même taille
+    // Ensure both vectors have the same size
     if (features1.size() != features2.size()) {
-        cerr << "Erreur: Les vecteurs n'ont pas la même taille." << endl;
-        return -1.0; // Ou utilisez une gestion d'erreur appropriée
+        cerr << "Error: Vectors do not have the same size." << endl;
+        return -1.0; // Or use appropriate error handling
     }
 
     for (int i = 0; i < features1.size(); ++i) {
@@ -47,13 +42,14 @@ double calculateDistance(vector<float> features1, vector<float> features2)
     return sqrt(sum);
 }
 
+// Function to calculate Manhattan distance between two feature vectors
 double calculateDistanceManhattan(vector<float> features1, vector<float> features2)
 {
     double sum = 0.0;
-    // Assurez-vous que les deux vecteurs ont la même taille
+    // Ensure both vectors have the same size
     if (features1.size() != features2.size()) {
-        cerr << "Erreur: Les vecteurs n'ont pas la même taille." << endl;
-        return -1.0; // Ou utilisez une gestion d'erreur appropriée
+        cerr << "Error: Vectors do not have the same size." << endl;
+        return -1.0; // Or use appropriate error handling
     }
 
     for (int i = 0; i < features1.size(); ++i) {
@@ -62,34 +58,33 @@ double calculateDistanceManhattan(vector<float> features1, vector<float> feature
     return sum;
 }
 
-
-// Used to sort an array of points by increasing
-// order of distance
-bool comparison(Point a, Point b)
+// Used to sort an array of ImageDatas by increasing order of distance
+bool comparison(ImageData a, ImageData b)
 {
     return (a.distance < b.distance);
 }
-int classifyAPoint(Point arr[], int n, int k, Point p)
+
+int classifyImageData(ImageData arr[], int n, int k, ImageData p)
 {
-    // Remplir les distances de tous les points par rapport à p
+    // Fill distances of all ImageDatas from p
     for (int i = 0; i < n; i++)
     {
         arr[i].distance = calculateDistance(arr[i].features, p.features);
     }
 
-    // Trier les points par distance par rapport à p
+    // Sort ImageDatas by distance from p
     sort(arr, arr + n, comparison);
 
-    // Utiliser un tableau pour stocker les fréquences de chaque classe
+    // Use an array to store frequencies of each class
     vector<int> frequencies(numClasses, 0);
 
-    // Considérer les premiers k éléments et compter les fréquences de chaque classe
+    // Consider first k elements and count frequencies of each class
     for (int i = 0; i < k; i++)
     {
         frequencies[arr[i].val]++;
     }
 
-    // Trouver la classe avec la fréquence la plus élevée parmi les k voisins
+    // Find the class with the highest frequency among k neighbors
     int maxFreq = frequencies[0];
     int predictedClass = 0;
     for (int i = 1; i < numClasses; ++i)
@@ -104,134 +99,101 @@ int classifyAPoint(Point arr[], int n, int k, Point p)
     return predictedClass;
 }
 
- 
 // Driver code
 int main()
 {
-    int numData = map1[nameClasse];
+    int numData = map1[className];
 
     DIR *dr;
     struct dirent *en;
-    dr = opendir(nameClasse.c_str()); // Ouvrir le répertoire
-    vector<Point> arr; // Utiliser un vecteur pour stocker les points
+    dr = opendir(className.c_str()); // Open the directory
+    vector<ImageData> arr; // Use a vector to store the ImageDatas
 
     if (dr) {
         while ((en = readdir(dr)) != NULL) {
-            if (en->d_name[0] == '.') continue; // Ignorer les fichiers cachés
+            if (en->d_name[0] == '.') continue; // Ignore hidden files
 
-            // Créer le chemin complet du fichier
-            string filePath = nameClasse+"/" + string(en->d_name);
+            // Create the full file path
+            string filePath = className + "/" + string(en->d_name);
 
-            // Ouvrir le fichier en mode lecture
+            // Open the file in read mode
             ifstream file(filePath);
             if (file.is_open()) {
-                Point p(numData);
-                int classLabel = stoi(en->d_name + 1); // Extraire le numéro de classe du nom du fichier
+                ImageData p(numData);
+                int classLabel = stoi(en->d_name + 1); // Extract class number from the file name
                 p.val = classLabel;
-                
-                // Lire les 16 valeurs du fichier et les stocker dans le tableau features
+
+                // Read the values from the file and store them in the features array
                 for (int i = 0; i < numData; ++i) {
                     float value;
                     file >> value;
                     p.features[i] = value;
                 }
 
-                // Ajouter le point au vecteur
+                // Add the ImageData to the vector
                 arr.push_back(p);
 
-                // Fermer le fichier
+                // Close the file
                 file.close();
             }
         }
-        closedir(dr); // Fermer le répertoire
-    } 
+        closedir(dr); // Close the directory
+    }
 
 //////// TEST PART ////////
 
-    dr = opendir((nameClasse + "Test").c_str()); // Ouvrir le répertoire
-    vector<Point> test; // Utiliser un vecteur pour stocker les points
+    dr = opendir((className + "Test").c_str()); // Open the directory
+    vector<ImageData> test; // Use a vector to store the test ImageDatas
 
     if (dr) {
         while ((en = readdir(dr)) != NULL) {
-            if (en->d_name[0] == '.') continue; // Ignorer les fichiers cachés
+            if (en->d_name[0] == '.') continue; // Ignore hidden files
 
-            // Créer le chemin complet du fichier
-            string filePath = nameClasse + "Test"+"/" + string(en->d_name);
+            // Create the full file path
+            string filePath = className + "Test" + "/" + string(en->d_name);
 
-            // Ouvrir le fichier en mode lecture
+            // Open the file in read mode
             ifstream file(filePath);
             if (file.is_open()) {
-                Point p(numData);
-                int classLabel = stoi(en->d_name + 1); // Extraire le numéro de classe du nom du fichier
+                ImageData p(numData);
+                int classLabel = stoi(en->d_name + 1); // Extract class number from the file name
                 p.val = classLabel;
 
-                // Lire les 16 valeurs du fichier et les stocker dans le tableau features
+                // Read the values from the file and store them in the features array
                 for (int i = 0; i < numData; ++i) {
                     float value;
                     file >> value;
                     p.features[i] = value;
                 }
 
-                // Ajouter le point au vecteur
+                // Add the ImageData to the vector
                 test.push_back(p);
 
-                // Fermer le fichier
+                // Close the file
                 file.close();
             }
         }
-        closedir(dr); // Fermer le répertoire
-    } 
+        closedir(dr); // Close the directory
+    }
 
-   // Faites varier la valeur de k de 1 à 5
+   // Vary the value of k from 1 to 6
     for (int k = 1; k <= 6; ++k) {
-        int correctClassifications = 0; // Réinitialiser le compteur des classifications correctes
+        int correctClassifications = 0; // Reset the correct classification counter
 
-        for (vector<Point>::iterator t = test.begin(); t != test.end(); ++t) {
-            int c = classifyAPoint(arr.data(), arr.size(), k, *t);
+        for (vector<ImageData>::iterator t = test.begin(); t != test.end(); ++t) {
+            int c = classifyImageData(arr.data(), arr.size(), k, *t);
             //printf("k=%d, Classified value: %d, Actual value: %d. ", k, c, t->val);
             if (c == t->val) {
                 //printf("Classification Correct\n");
-                correctClassifications++; // Incrémenter le compteur des classifications correctes
+                correctClassifications++; // Increment the correct classification counter
             } else {
                 //printf("Classification Incorrect\n");
             }
         }
 
-        // Calculer et afficher le pourcentage de réussite pour cette valeur de k
+        // Calculate and print the success percentage for this value of k
         double successPercentage = (static_cast<double>(correctClassifications) / test.size()) * 100;
-        printf("Pourcentage de réussite pour k=%d : %.2f%%\n", k, successPercentage);
+        printf("Success Percentage for k=%d: %.2f%%\n", k, successPercentage);
     }
-
-
-
-
-    /*
-    // Créer le point de test (vous devez remplir ses valeurs à partir du dernier fichier)
-    Point testPoint;
-    // Remplir les valeurs de testPoint à partir du dernier fichier ici...
-
-
-    // Créer le chemin complet du fichier
-    string filePath = "F0Test/s09n008.F0";
-
-    // Ouvrir le fichier en mode lecture
-    ifstream file(filePath);
-    if (file.is_open()) {
-        // Lire les 16 valeurs du fichier et les stocker dans le tableau features
-        for (int i = 0; i < 128; ++i) {
-            float value;
-            file >> value;
-            testPoint.features[i] = value;
-            std::cout<<testPoint.features[i]<<endl;
-        }
-
-        // Fermer le fichier
-        file.close();
-    }
-    // Parameter to decide group of the testing point
-    int k = 5;
-    printf ("The value classified to unknown point is %d.\n", classifyAPoint(arr.data(), arr.size(), k, testPoint));
-
-*/
     return 0;
 }
