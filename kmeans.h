570a534423c6ae861ff7b3f943cdf3bd6197172f
CookieCreams <148908423+CookieCreams@users.noreply.github.com>
2023-11-19 22:06:33 +0100

    Update kmeans.h

--- kmeans.h
+++ kmeans.h
@@ -6,10 +6,10 @@
 #include "UtilMethods.h"
 
 // Function declarations
-void kmeans(vector<ImageData>& data, int k, vector<vector<float>>& centroids, int max_iterations);
-void update_centroids(vector<ImageData>& data, vector<vector<float>>& centroids);
-void assign_data_to_clusters(vector<ImageData>& data, vector<vector<float>>& centroids);
-void init_centroids(vector<vector<float>>& centroids, const vector<ImageData>& data, int k);
+void kmeans(vector<ImageData>& data, int k, vector<centroid>& centroids, int max_iterations);
+void update_centroids(vector<ImageData>& data, vector<centroid>& centroids);
+void assign_data_to_clusters(vector<ImageData>& data, vector<centroid>& centroids);
+void init_centroids(vector<centroid>& centroids, const vector<ImageData>& data, int k);
 
 
 #endif // KNN_H
